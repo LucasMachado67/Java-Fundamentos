7257916c1ec9c8f849d555a1da56fc54b23fa126
Lucas Edson Machado <146786011+LucasMachado67@users.noreply.github.com>
2024-12-14 08:22:50 -0300

    Adicionando as aulas do java Fundamentos

---
+++ Aula03/ExemploWhile.java
@@ -0,0 +1,15 @@
+public class ExemploWhile {
+
+    public static void main(String[] args) {
+        
+        int indice = 0;
+        while(indice < 10){
+            System.out.println("Olá mundo!!");
+
+            // indice = 0 + 1 => 1
+            // indice = 1 + 1 => 2
+            // indice = 2 + 1 => 3
+            indice = indice + 1;
+        }
+    }
+}   
